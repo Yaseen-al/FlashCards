e6257601ba4a621e2d6a4502de0cc43201913a94
Yaseen-al <yaseenaldellesh@ac.c4q.nyc>
2018-02-13 18:29:32 -0500

    added the coreData models and extension

---
+++ FlashCards/Models/Category+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Category+Extension.swift
+//  FlashCards
+//
+//  Created by C4Q on 2/13/18.
+//  Copyright © 2018 QI. All rights reserved.
+//
+
+import Foundation
+extension Category{
+    //TODO make a convenience intializers as well as to JSON functions
+}
